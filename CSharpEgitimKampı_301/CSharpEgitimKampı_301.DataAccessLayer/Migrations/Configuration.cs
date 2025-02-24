057dde0cf2336c0287c4212de516140f294ea0dd
burakcan aksoy <aksoyburak@gmail.com>
2025-02-24 16:38:35 +0300

    to ef later

---
+++ CSharpEgitimKampı_301/CSharpEgitimKampı_301.DataAccessLayer/Migrations/Configuration.cs
@@ -0,0 +1,23 @@
+﻿namespace CSharpEgitimKampı_301.DataAccessLayer.Migrations
+{
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<CSharpEgitimKampı_301.DataAccessLayer.Context.KampContext>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = true;
+        }
+
+        protected override void Seed(CSharpEgitimKampı_301.DataAccessLayer.Context.KampContext context)
+        {
+            //  This method will be called after migrating to the latest version.
+
+            //  You can use the DbSet<T>.AddOrUpdate() helper extension method
+            //  to avoid creating duplicate seed data.
+        }
+    }
+}
---
+++ CSharpEgitimKampı_301/CSharpEgitimKampı_301.EntityLayer/Concrete/Customer.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpEgitimKampı_301.EntityLayer.Concrete
+{
+    public class Customer
+    {
+
+        public int CustomerId { get; set; }
+
+        public string CustomerName { get; set; }
+
+        public string CustomerSurname { get; set; }
+
+        public string CustomerEmail { get; set; }
+
+        public string CustomerPhone { get; set; }
+
+        public string CustomerCity { get; set; }
+
+        public string CustomerDistrict { get; set; }
+
+        public List<Order> Orders { get; set; }
+
+        public bool CustomerStatus { get; set; }   
+    }
+}
